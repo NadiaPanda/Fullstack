5cf26373c776229de5ede115357d7b79ded8d567
Pandaren <misterpanda93@gmail.com>
2021-12-07 22:58:21 +0700

    studying

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "Hello, World!";
+echo '<br>';
+echo 'Hello!';
+echo '<br>';
+echo '<button>Click!</button>';
+echo '<br>';
+echo date('d.m.Y H:i:s');
+echo '<br>';
+echo 2+13;
+echo '<br>';
+echo 11 % 3;
+echo '<br>';
+
+$burger = 4.95;
+$coctail = 1.95;
+$coca = 0.85;
+$allFood = ($burger * 2) + $coctail + $coca; 
+$tea = ($allFood) * 0.16;
+$nds = ($tea + $allFood) * 0.075;
+$total = $tea + $allFood + $nds;
+echo $total;
+
+
+echo '<br>';
+echo $tea;
+echo '<br>';
+echo $allFood;
+
